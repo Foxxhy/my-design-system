3d775d22062cecd1a169095194dc73f733c91543
Fr93562 <francois.mackowski@gmail.com>
2024-04-25 00:02:38 +0200

    fix(package): fix CSS class

--- src/client/components/badgeSection/BadgeSection.tsx
+++ src/client/components/badgeSection/BadgeSection.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { configExample } from '../../config';
 
 const BadgeSection = () => {
@@ -6,8 +7,8 @@ const BadgeSection = () => {
     return (
         <div className="f-flex f-flex-row f-flex-wrap f-gap-6 f-margin-bottom-16">
             <span className="f-badge-success">{short}</span>
-            <span className="f-badge-warning">{medium}</span>
-            <span className="f-badge-danger">{long}</span>
+            <span className="f-badge-outline-warning">{medium}</span>
+            <span className="f-badge-outline-danger">{long}</span>
         </div>
     )
 }
